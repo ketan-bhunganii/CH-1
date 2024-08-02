48c663b096013b8a21886ed38a6968c4d461546a
ketan-bhunganii <ketanbhungani1933@gmail.com>
2024-08-02 10:29:45 +0530

    c-language

---
+++ CHA1Q1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("\tname:-ketan bhungani\n");
+	printf("age :- 24\n\t");
+	printf("school name :- baps\n");
+	getch();
+
+
+
+}
---
+++ CHA1Q4.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdio.h>
+main()
+{
+	clrscr();
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*    *       *\n");
+	printf("*   *  *    *\n");
+	printf("*  *    *  *\n");
+	printf("* *      **\n");
+	printf("**\n");
+	printf("*");
+	getch();
+
+
+
+
+
+
+}
